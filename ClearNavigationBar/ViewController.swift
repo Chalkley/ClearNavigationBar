b58166c5c51cf0ed8be0d705002aa512b7670610
Jason Chalkley <jaychalks@gmail.com>
2018-12-11 12:38:07 +0000

    Initial Commit

---
+++ ClearNavigationBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearNavigationBar
+//
+//  Created by Jason Chalkley on 11/12/2018.
+//  Copyright © 2018 Jason Chalkley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
